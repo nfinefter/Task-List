423fdbe4344f8709961b3792e6791c3365e59f75
Nicholas Finefter <nfinefter@gmail.com>
2025-11-04 21:11:54 -0800

    Made minor fixes

--- api/index.js
+++ api/index.js
@@ -1,63 +1,64 @@
 import express from "express";
-import { fetchTasks, createTasks, updateTasks, deleteTasks } from"./task.js";
 import serverless from "serverless-http";
 import cors from "cors";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
+
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
-if (process.env.DEVELOPMENT){
-    app.use(cors());
+if (process.env.DEVELOPMENT) {
+  app.use(cors());
 }
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/task.js", async (req, res) => {
+app.get("/task", async (req, res) => {
   try {
     const tasks = await fetchTasks();
 
     res.send(tasks.Items);
   } catch (err) {
-    res.status(400).send({ error: `Could not fetch tasks: ${err}` });
+    res.status(400).send(`Error fetching tasks: ${err}`);
   }
 });
 
-app.post("/task.js", async (req, res) => {
+app.post("/task", async (req, res) => {
   try {
-    const tasks = req.body;
+    const task = req.body;
 
-    const response = await createTasks(tasks);
+    const response = await createTasks(task);
 
     res.send(response);
   } catch (err) {
-    res.status(400).send({ error: `Could not create tasks: ${err}` });
+    res.status(400).send(`Error creating tasks: ${err}`);
   }
 });
 
-app.put("/task.js", async (req, res) => {
-   try {
-    const tasks = req.body;
+app.put("/task", async (req, res) => {
+  try {
+    const task = req.body;
 
-    const response = await updateTasks(tasks);
+    const response = await updateTasks(task);
 
     res.send(response);
   } catch (err) {
-    res.status(400).send({ error: `Could not update tasks: ${err}` });
+    res.status(400).send(`Error updating tasks: ${err}`);
   }
 });
 
-app.delete("/task.js/:id", async (req, res) => {
+app.delete("/task/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     const response = await deleteTasks(id);
 
     res.send(response);
   } catch (err) {
-    res.status(400).send({ error: `Could not delete tasks: ${err}` });
+    res.status(400).send(`Error deleting tasks: ${err}`);
   }
 });
 
